ce8445043215334be0472e1cb22c3fddea9a4ac1
kekko code <102719668+Kekko0code@users.noreply.github.com>
2022-03-31 08:48:04 -1200

    Add files via upload
    es di uso di while

---
+++ while_es.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    system("color a");
+    int x,y,z;
+    x = 0;
+    cin>>y;
+    while(x<=y)
+    {
+        cout<<x<<" ";
+        x = x+1;
+    }
+
+
+    return 0;
+
+}
