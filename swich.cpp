3fb47b095df0f79b4d6cc0f19f988098075a253e
kekko code <102719668+Kekko0code@users.noreply.github.com>
2022-03-31 08:49:52 -1200

    Add files via upload
    es di switch case

---
+++ swich.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin>>x;
+	switch (x)
+	{
+		case 1:
+			cout<<"uno";
+			break;
+		case 2:
+			cout<<"due";
+			break;
+		case 3:
+			cout<<"tre";
+			break;
+		case 4:
+			cout<<"quatro";
+			break;
+		case 5:
+			cout<<"cinque";
+			break;
+		case 6:
+			cout<<"sei";
+			break;
+		case 7:
+			cout<<"sette";
+			break;
+		case 8:
+			cout<<"otto";
+			break;
+		case 9:
+			cout<<"nove";
+			break;
+		case 10:
+			cout<<"dieci";
+			break;
+		default:
+			cout<<"il numero e'  maggiore di dieci";
+			break;
+	}
+	return 0;
+}
