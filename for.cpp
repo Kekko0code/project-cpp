1bd2cde895179b35ca2d10a99afe4ad766f191d0
kekko code <102719668+Kekko0code@users.noreply.github.com>
2022-03-31 08:48:38 -1200

    Add files via upload
    es di for

---
+++ for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        cout<<"ciao"<<endl;
+        Sleep(1000);
+    }
+    return 0;
+}
